074ee46d40187db6e9a2d7324d98b46a11444e1e
Jhoner Pineda <39860554+Zeroluffs@users.noreply.github.com>
2020-10-09 10:01:40 -0500

    FirebaseFavorRepository.kt agregado

--- app/src/main/java/com/kebinr/karmaaplication/model/Favor.kt
+++ app/src/main/java/com/kebinr/karmaaplication/model/Favor.kt
@@ -2,4 +2,5 @@ package com.kebinr.karmaaplication.model
 import com.google.firebase.firestore.IgnoreExtraProperties
 
 @IgnoreExtraProperties
-data class Favor (val id: Int? = 0, val text: String? = "", val user: String? = "")
+data class Favor (val user_asking: String? = "",val user_toDo: String?="", val type: String? = "", val status: String? , val fotocopy: String?, val buyKM5 : String?="" ,val quantityKM5: Int? =0, val delivery: String?="")
+
--- app/src/main/java/com/kebinr/karmaaplication/model/User.kt
+++ app/src/main/java/com/kebinr/karmaaplication/model/User.kt
@@ -1,4 +1,4 @@
 package com.kebinr.karmaaplication.model
 
-class User (val user_asking: String? = "",val user_toDo: String?="", val type: String? = "", val status: String? , val fotocopy: String?, val buyKM5 : String?="" ,val quantityKM5: Int? =0, val delivery: String?="" ){
+class User (val email: String? = "", val key: String? = "" , val karma: String?){
 }
