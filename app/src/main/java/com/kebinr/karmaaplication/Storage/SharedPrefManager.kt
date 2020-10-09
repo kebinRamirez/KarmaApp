04e51d7125cef4266a84963a11314cc25e52ef86
Jhoner Pineda <39860554+Zeroluffs@users.noreply.github.com>
2020-10-09 10:30:13 -0500

    sharedpreferences agregado

---
+++ app/src/main/java/com/kebinr/karmaaplication/Storage/SharedPrefManager.kt
@@ -0,0 +1,60 @@
+package com.kebinr.karmaaplication.Storage
+
+import android.content.Context
+import com.kebinr.karmaaplication.model.User
+
+class SharedPrefManager private constructor(private val mCtx: Context) {
+
+    val isLoggedIn: Boolean
+        get() {
+            val sharedPreferences = mCtx.getSharedPreferences(SHARED_PREF_NAME, Context.MODE_PRIVATE)
+            return sharedPreferences.getInt("id", -1) != -1
+        }
+
+    val user: User
+        get() {
+            val sharedPreferences = mCtx.getSharedPreferences(SHARED_PREF_NAME, Context.MODE_PRIVATE)
+            return User(
+                sharedPreferences.getString("email", null)!!,
+                sharedPreferences.getString("key", null)!!,
+                sharedPreferences.getString("karma", null)!!
+
+
+
+                )
+        }
+
+
+    fun saveUser(user: User) {
+
+        val sharedPreferences = mCtx.getSharedPreferences(SHARED_PREF_NAME, Context.MODE_PRIVATE)
+        val editor = sharedPreferences.edit()
+        editor.putString("email", user.email)
+        editor.putString("key", user.key)
+        editor.putString("karma", user.karma)
+
+
+        editor.apply()
+
+    }
+
+    fun clear() {
+        val sharedPreferences = mCtx.getSharedPreferences(SHARED_PREF_NAME, Context.MODE_PRIVATE)
+        val editor = sharedPreferences.edit()
+        editor.clear()
+        editor.apply()
+    }
+
+    companion object {
+        private val SHARED_PREF_NAME = "my_shared_preff"
+        private var mInstance: SharedPrefManager? = null
+        @Synchronized
+        fun getInstance(mCtx: Context): SharedPrefManager {
+            if (mInstance == null) {
+                mInstance = SharedPrefManager(mCtx)
+            }
+            return mInstance as SharedPrefManager
+        }
+    }
+
+}
