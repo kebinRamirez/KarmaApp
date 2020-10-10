ed5078c13021ae345f4276964f2580ab06337147
kebinr <kebinr@uninorte.edu.co>
2020-10-10 02:37:21 -0500

    implementacion de menu desplegable

--- app/src/main/java/com/kebinr/karmaaplication/ui/content/MainActivity.kt
+++ app/src/main/java/com/kebinr/karmaaplication/ui/content/MainActivity.kt
@@ -2,14 +2,50 @@ package com.kebinr.karmaaplication.ui.content
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.core.view.GravityCompat
 import com.kebinr.karmaaplication.R
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.android.synthetic.main.activity_main.*
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         setContentView(R.layout.activity_main)
-        setSupportActionBar(findViewById(R.id.toolbar2))
+        setSupportActionBar(findViewById(R.id.toolbar))
+        navigation_view.setNavigationItemSelectedListener {
+            when(it.itemId){
+                R.id.nav_home->{
+                    true
+                }
+            }
+            true
+        }
+        val drawerToggle = ActionBarDrawerToggle(this, drawer, R.string.open, R.string.close)
+        drawer.addDrawerListener(drawerToggle)
+        drawerToggle.syncState()
+
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+    }
+    override fun onBackPressed() {
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START)
+        } else {
+            super.onBackPressed()
+        }
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        super.onOptionsItemSelected(item)
+        return when (item.itemId) {
+            android.R.id.home -> {
+                drawer.openDrawer(GravityCompat.START)
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
     }
 }
